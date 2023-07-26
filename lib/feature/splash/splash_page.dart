258d1dfbb046f9ca502092a83926af91b071117a
toni <antonyngethe@gmail.com>
2023-07-26 15:33:48 +0300

    feat: add splash page

--- lib/feature/splash/splash_page.dart
+++ lib/feature/splash/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:bstock/app/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashPage extends StatelessWidget {
@@ -11,9 +12,38 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        alignment: Alignment.center,
-        child: SvgPicture.asset(Assets.appIcon),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              Assets.appIcon,
+              height: 80,
+              width: 80,
+            ),
+            SizedBox(
+              height: 8.h,
+            ),
+            RichText(
+              text: TextSpan(
+                  text: "B",
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold, color: Colors.black),
+                  children: [
+                    TextSpan(
+                        text: "-",
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold, color: Colors.black)),
+                    TextSpan(
+                        text: "Stock",
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.primary))
+                  ]),
+            )
+          ],
+        ),
       ),
     );
   }
